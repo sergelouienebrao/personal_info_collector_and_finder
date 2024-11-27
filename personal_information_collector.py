3b59383711255f9ee91e06ad4648cfcf2b5fa89d
Serge <sergelouienebrao@gmail.com>
2024-11-27 22:33:49 +0800

    pseudo

---
+++ personal_information_collector.py
@@ -0,0 +1,9 @@
+#create the file name where data will be stored
+
+#make a loop to allow multiple entries
+ 
+#prompt for user inputs
+
+#save the info into the file 
+
+#ask if theres another input
