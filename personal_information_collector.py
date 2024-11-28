0428deacfa2ad5203d345359f43811bac6910f0f
Serge <sergelouienebrao@gmail.com>
2024-11-28 23:47:49 +0800

    prompt inputs

--- personal_information_collector.py
+++ personal_information_collector.py
@@ -1,6 +1,23 @@
 #create the file name where data will be stored
 
 #make a loop to allow multiple entries
+while True:
+    try: 
+        full_name = input("Full name: ")
+        address = input("Address: ")
+        age = int(input("Age: "))
+        birthday = int(input("Birthday (MM/DD/YY): "))
+        contact_number = int(input("Phone number: "))
+
+    except:
+        print("Invalid input, try again.")
+    break
+
+print(full_name)
+print(age)
+print(birthday)
+print(contact_number)
+
  
 #prompt for user inputs
 
