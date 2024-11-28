495ef1382a39d6f535c849350ede011ca894d2a8
Serge <sergelouienebrao@gmail.com>
2024-11-29 01:27:49 +0800

    inputs saved into txt file

--- personal_information_collector.py
+++ personal_information_collector.py
@@ -1,26 +1,29 @@
 #create the file name where data will be stored
+file_name = "personal_info_collection.txt"
 
 #make a loop to allow multiple entries
 while True:
+#prompt for user inputs
     try: 
         full_name = input("Full name: ")
         address = input("Address: ")
         age = int(input("Age: "))
-        birthday = int(input("Birthday (MM/DD/YY): "))
+        birthday = (input("Birthday (MM/DD/YY): "))
         contact_number = int(input("Phone number: "))
+
+#save the info into the file
+        with open(file_name, "a") as file:
+            file.write(f"Full name: {full_name}\n")
+            file.write(f"Address: {address}\n")
+            file.write(f"Age: {age}\n")
+            file.write(f"Birthday: {birthday}\n")
+            file.write(f"Contact Number: {contact_number}\n")
 
     except:
         print("Invalid input, try again.")
-    break
+    
 
-print(full_name)
-print(age)
-print(birthday)
-print(contact_number)
 
- 
-#prompt for user inputs
 
-#save the info into the file 
 
-#ask if theres another input
+#ask user if theres another input
