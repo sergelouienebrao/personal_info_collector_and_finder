907bf468b612e6b899148e1b61eb2f82fee66989
Serge <sergelouienebrao@gmail.com>
2024-11-29 22:49:23 +0800

    ask for another input

--- personal_information_collector.py
+++ personal_information_collector.py
@@ -4,26 +4,22 @@ file_name = "personal_info_collection.txt"
 #make a loop to allow multiple entries
 while True:
 #prompt for user inputs
-    try: 
-        full_name = input("Full name: ")
-        address = input("Address: ")
-        age = int(input("Age: "))
-        birthday = (input("Birthday (MM/DD/YY): "))
-        contact_number = int(input("Phone number: "))
+
+    full_name = input("Full name: ")
+    address = input("Address: ")
+    age = int(input("Age: "))
+    birthday = (input("Birthday (MM/DD/YY): "))
+    contact_number = int(input("Phone number: "))
 
 #save the info into the file
-        with open(file_name, "a") as file:
-            file.write(f"Full name: {full_name}\n")
-            file.write(f"Address: {address}\n")
-            file.write(f"Age: {age}\n")
-            file.write(f"Birthday: {birthday}\n")
-            file.write(f"Contact Number: {contact_number}\n")
-
-    except:
-        print("Invalid input, try again.")
-    
-
-
-
-
+    with open(file_name, "a") as file:
+        file.write(f"Full name: {full_name}\n")
+        file.write(f"Address: {address}\n")
+        file.write(f"Age: {age}\n")
+        file.write(f"Birthday: {birthday}\n")
+        file.write(f"Contact Number: {contact_number}\n")
 #ask user if theres another input
+    another_input = input("Do you want to add another person? (yes/no): ")
+    if another_input != "yes":
+      print("Input finished, thank you!")
+      break
