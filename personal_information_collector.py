45775189913bcf012c0ee2750dea74e02a6e1ec8
Serge <sergelouienebrao@gmail.com>
2024-11-29 23:21:52 +0800

    fixed input validation

--- personal_information_collector.py
+++ personal_information_collector.py
@@ -5,17 +5,42 @@ print("Personal Information Collector")
 
 #make a loop to allow multiple entries
 while True:
-#prompt for user inputs
+#prompt for user input
     full_name = input("Full name: ")
+    while not full_name:
+        print("Full name cannot be empty. Please try again.")
+        full_name = input("Full name: ")
+
     address = input("Address: ")
-    age = int(input("Age: "))
-    birthday = (input("Birthday (MM/DD/YY): "))
-    contact_number = int(input("Phone number: "))
+    while not address:
+        print("Address cannot be empty. Please try again.")
+        address = input("Address: ")
+
+    while True:
+        try:
+           age_input = int(input("Age: "))
+           if age_input > 0:
+               break
+        except:
+            print("Invalid input. Please enter a valid age as a positive number.")
+
+    birthday = input("Birthday (MM/DD/YY): ")
+    while not birthday:
+        print("Birthday cannot be empty. Please try again.")
+        birthday = input("Birthday (MM/DD/YY): ")
+
+    while True:
+        contact_number = input("Phone number: ")
+        if len(contact_number) == 11:  
+         break  
+        else:
+          print("Invalid input. Please enter a valid phone number that is exactly 11 digits.")
+
 #save the info into the file
     with open(file_name, "a") as file:
         file.write(f"Full name: {full_name}\n")
         file.write(f"Address: {address}\n")
-        file.write(f"Age: {age}\n")
+        file.write(f"Age: {age_input}\n")
         file.write(f"Birthday: {birthday}\n")
         file.write(f"Contact Number: {contact_number}\n")
         file.write("\n")
