b4f01cf56047db1746d53c7e9a51ac77cdb4e8da
Serge <sergelouienebrao@gmail.com>
2024-11-30 19:45:23 +0800

    finished code yey

--- search_personal_information.py
+++ search_personal_information.py
@@ -1,4 +1,6 @@
 file_name = "personal_info_collection.txt"
+
+print("Search Personal Information")
 #ask user for the name to search
 while True:
     name_search = input("Enter the name to search: ").strip()
