5d51b52acf469885a9a6b3fd967397cf7354d621
Serge <sergelouienebrao@gmail.com>
2024-11-30 16:58:09 +0800

    open file

--- search_personal_information.py
+++ search_personal_information.py
@@ -2,9 +2,9 @@ file_name = "personal_info_collection.txt"
 #ask user for the name to search
 while True:
     name_search = input("Enter the name to search: ")
-
 #open the file
-
+    with open(file_name, "r") as file:
+        entries = file.read().strip("-----\n")
 
 #search for the name
 
