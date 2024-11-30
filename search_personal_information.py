a13347fee0c838da6909c07bd4b61a5fc42fc50a
Serge <sergelouienebrao@gmail.com>
2024-11-30 18:08:14 +0800

    dont know na

--- search_personal_information.py
+++ search_personal_information.py
@@ -2,10 +2,18 @@ file_name = "personal_info_collection.txt"
 #ask user for the name to search
 while True:
     name_search = input("Enter the name to search: ")
+    found = False
 #open the file
     with open(file_name, "r") as file:
         entries = file.read().strip("-----\n")
-
-#search for the name
+        for entry in entries:
+            if f"Full Name: {name_search}" in entry:
+              print("\nInformation Found:")
+              print(entry.strip())
+              found = True
+              break
+    if not found:
+        print(f"No information found for '{name_search}'.")
 
 #ask user for another search
+
